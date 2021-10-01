bd9d1409fd7eda70746c25eb731dda587375240f
Hidenori Miyoshi <miyohide@gmail.com>
2021-10-01 21:10:32 +0900

    create initial files using Spring Initializr

---
+++ src/main/java/com/github/miyohide/springbootretrydb/SpringBootRetryDbApplication.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.springbootretrydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRetryDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRetryDbApplication.class, args);
+    }
+
+}
