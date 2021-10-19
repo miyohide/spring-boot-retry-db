288688f0847451a257bbb7e258a813a968820c6d
Hidenori Miyoshi <miyohide@gmail.com>
2021-10-19 20:43:28 +0900

    copy directory

---
+++ non_retry_sample/README.md
@@ -0,0 +1,4 @@
+# これは何か
+
+Spring Bootを使ってAzure Database for PostgreSQLへのリトライ処理を実装したサンプルです。
+
---
+++ non_retry_sample/src/main/java/com/github/miyohide/springbootretrydb/config/RetryableDatabasePostProcessor.java
@@ -0,0 +1,32 @@
+package com.github.miyohide.springbootretrydb.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Order(value = Ordered.HIGHEST_PRECEDENCE)
+@Component
+public class RetryableDatabasePostProcessor implements BeanPostProcessor {
+    private static final Logger log =
+            LoggerFactory.getLogger(RetryableDatabasePostProcessor.class);
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof DataSource) {
+            log.info("-----> configuring a retryable datasource for beanName = {}", beanName);
+            return new RetryableDataSource((DataSource) bean);
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+}
