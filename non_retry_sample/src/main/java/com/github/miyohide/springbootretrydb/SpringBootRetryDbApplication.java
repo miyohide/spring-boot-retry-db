ffd675b43fe436239fd1a8cfbda4df465e08ec87
Hidenori Miyoshi <miyohide@gmail.com>
2021-10-20 20:56:29 +0900

    create service class

--- non_retry_sample/src/main/java/com/github/miyohide/springbootretrydb/SpringBootRetryDbApplication.java
+++ non_retry_sample/src/main/java/com/github/miyohide/springbootretrydb/SpringBootRetryDbApplication.java
@@ -1,26 +1,14 @@
 package com.github.miyohide.springbootretrydb;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.time.OffsetDateTime;
 
 @SpringBootApplication
 public class SpringBootRetryDbApplication implements CommandLineRunner {
-    private static final Logger log =
-            LoggerFactory.getLogger(SpringBootRetryDbApplication.class);
-
-    @Value("${app.records.num:10}")
-    private int RECORDS_NUM;
-
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    private MyService myService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRetryDbApplication.class, args);
@@ -28,28 +16,7 @@ public class SpringBootRetryDbApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        log.info("Start command line app...");
-        for (int i = 0; i < RECORDS_NUM; i++) {
-            String first_name = String.format("Josh%05d", i);
-            String last_name = String.format("hogehoge%05d", i);
-            log.info("Insert record. i = [" + i + "], first_name = [" + first_name + "], last_name = [" + last_name + "]");
-            jdbcTemplate.update("INSERT INTO customers(first_name, last_name) VALUES (?, ?)", first_name, last_name);
-            try {
-                Thread.sleep(1_000);
-            } catch (InterruptedException e) {
-                log.info("Thread.sleep occurs InterruptedException. i = [" + i + "]");
-            }
-        }
-        log.info("Querying for customer records where first_name = 'Josh%': ");
-        jdbcTemplate.query("SELECT id, first_name, last_name, created_at FROM customers WHERE first_name LIKE ? ORDER BY id",
-                (rs, rowNum) -> new Customer(
-                        rs.getLong("id"),
-                        rs.getString("first_name"),
-                        rs.getString("last_name"),
-                        rs.getObject("created_at", OffsetDateTime.class)
-                ),
-                        new Object[] { "Josh%" })
-                        .forEach(customer -> log.info(customer.toString()));
-        log.info("End command line app");
+        myService.insert();
+        myService.printRecords();
     }
 }
