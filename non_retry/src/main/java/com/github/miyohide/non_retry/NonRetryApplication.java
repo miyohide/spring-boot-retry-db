91ae3ecf5ca5b579f48f1247667b2e55e711c538
Hidenori Miyoshi <miyohide@gmail.com>
2021-10-24 21:21:06 +0900

    create non_retry sample

---
+++ non_retry/src/main/java/com/github/miyohide/non_retry/NonRetryApplication.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.non_retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonRetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonRetryApplication.class, args);
+    }
+
+}
---
+++ non_retry/src/test/java/com/github/miyohide/non_retry/NonRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.non_retry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonRetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
